519e2492c38d3f1ddd6a2cfdec78720a2cd057c9
codeleaded <gnu.c.reaper@gmail.com>
2026-05-03 22:08:32 +0200

    update

--- src/Main.c
+++ src/Main.c
@@ -384,7 +384,7 @@ void Update(AlxWindow* w){
 	Figure_Render(&mario,&tv,WINDOW_STD_ARGS);
 
 	//String str = String_Format("MI:%d",nMaxIterations);
-	//CStr_RenderSizeAlxFont(WINDOW_STD_ARGS,&window.AlxFont,str.Memory,str.size,0.0f,0.0f,WHITE);
+	//CStr_RenderSizeAlxFont(WINDOW_STD_ARGS,&window.font,str.Memory,str.size,0.0f,0.0f,WHITE);
 	//String_Free(&str);
 }
 void Delete(AlxWindow* w){
